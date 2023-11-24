acdc22b8a43e1f32a826b58f4075595cb8ff74f8
Reetool16 <reetoolgupta16.com>
2023-11-24 16:37:28 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import Api from "./api";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import State from "./state";
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+      <State />
+      <Api />
+    </div>
+  );
+}
+
+export default App;
